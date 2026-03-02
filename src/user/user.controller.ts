d17ea13ba7b9773ee2c4ca535abca964b7cef9b7
nuttakit_kongkaew <68010334@kmitl.ac.th>
2026-03-02 10:08:38 +0700

    test

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
-import { UserService } from './user.service';
+import { UserService, User } from './user.service';
 
 @Controller('users')
 export class UserController {
