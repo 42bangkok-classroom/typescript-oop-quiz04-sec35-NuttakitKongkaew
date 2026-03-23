1dbf334c77682d2935b656984d88e600f011bea7
nuttakit_kongkaew <68010334@kmitl.ac.th>
2026-03-23 23:52:18 +0700

    3

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,44 +1,12 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Param,
-  Post,
-  Query,
-  ValidationPipe,
-} from '@nestjs/common';
-import { UserService } from './user.service';
-import { IUser } from './user.interface';
-import { CreateUserDto } from './dto/create-user.dto';
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
 
-@Controller('users')
-export class UserController {
-  constructor(private readonly userService: UserService) {}
-
-  @Get('test')
-  test(): IUser[] {
-    return this.userService.test();
-  }
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
 
   @Get()
-  findAll(): IUser[] {
-    return this.userService.findAll();
-  }
-
-  @Get(':id')
-  findOne(
-    @Param('id') id: string,
-    @Query('fields') fields?: string,
-  ): Partial<IUser> {
-    const fieldArray = fields ? fields.split(',') : undefined;
-    return this.userService.findOne(id, fieldArray);
-  }
-
-  @Post()
-  create(
-    @Body(new ValidationPipe({ whitelist: true }))
-    dto: CreateUserDto,
-  ): IUser {
-    return this.userService.create(dto);
+  getHello(): string {
+    return this.appService.getHello();
   }
 }
