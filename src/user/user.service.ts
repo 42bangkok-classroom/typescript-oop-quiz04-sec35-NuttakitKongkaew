9e7755cdfee35ebe396e5c381d6513d65fced576
nuttakit_kongkaew <68010334@kmitl.ac.th>
2026-03-23 23:26:00 +0700

    3

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,14 +4,14 @@ import { IUser } from './user.interface';
 
 @Injectable()
 export class UserService {
+
   test(): IUser[] {
     return [];
   }
 
   findAll(): IUser[] {
     const data = fs.readFileSync('data/users.json', 'utf-8');
-    const users: IUser[] = JSON.parse(data);
-
-    return users;
+    return JSON.parse(data) as IUser[];
   }
+
 }
